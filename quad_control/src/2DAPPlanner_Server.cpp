fddc8aeaea66da967c0264a340ec0fd5feec3b3e
Michele Marolla <mic.marolla@gmail.com>
2020-12-10 10:14:03 +0100

    Added service definition for 2D planner

---
+++ quad_control/src/2DAPPlanner_Server.cpp
@@ -0,0 +1,9 @@
+/*
+ * 2DAPPlanner provides a service that realizes path planning via artificial
+ * potentials method, for a 2D known environment.
+ * The planning is realized considering the virtual force acting on the robot
+ * as a velocity, and the integration is realized through Euler method.
+ * Corresponding .srv file is 2DAPPlanner.srv
+ */
+
+
